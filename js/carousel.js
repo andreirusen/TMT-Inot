8df172f77d0531a109920e61634ed3830ba50918
Andrei Rusen <123008787+andreirusen@users.noreply.github.com>
2023-07-25 13:44:03 +0300

    update

---
+++ js/carousel.js
@@ -0,0 +1,31 @@
+// Rating Carousel
+var swiper = new Swiper(".swiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 2,
+    speed: 600,
+    loop: true,
+    coverflowEffect: {
+      rotate: 0,
+      stretch: 0,
+      depth: 100,
+      modifier: 3,
+      slideShadows: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: false,
+    },
+  });
+  
+  // Timer to change the slider every 4 seconds
+  setInterval(function() {
+    swiper.slideNext();
+  }, 4000);
+  
+  
