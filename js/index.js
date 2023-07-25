56af8b76962f0b9d2ebc4c240d47c302d064fe83
Volkersen <123008787+andreirusen@users.noreply.github.com>
2023-07-25 16:46:39 +0300

    update cookies

--- js/index.js
+++ js/index.js
@@ -59,12 +59,18 @@ function reveal() {
 // Cookie
 console.clear();
 ("use strict");
-(function () {
-  var offcanvas = document.getElementById("offcanvas"),
-    bs_offcanvas = new bootstrap.Offcanvas(offcanvas);
 
-  bs_offcanvas.show();
+(function () {
+  var offcanvas = document.getElementById("offcanvas");
+
+  if (!sessionStorage.getItem("offcanvasShown")) {
+    var bs_offcanvas = new bootstrap.Offcanvas(offcanvas);
+    bs_offcanvas.show();
+
+    sessionStorage.setItem("offcanvasShown", true);
+  }
 })();
+
 
 // Google Map
 let map;
