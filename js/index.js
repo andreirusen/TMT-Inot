0bf6e4da92cb1fd09349e923b4816e5e0adbda20
Andrei Rusen <123008787+andreirusen@users.noreply.github.com>
2024-01-06 14:50:30 +0200

    update carusel intrare + footer

--- js/index.js
+++ js/index.js
@@ -95,18 +95,18 @@ console.clear();
 })();
 
 // Google Map
-let map;
-async function initMap() {
-  const { Map } = await google.maps.importLibrary("maps");
-  map = new Map(document.getElementById("map"), {
-    center: { lat: 44.434831044470364, lng: 26.147003026598945 },
-    zoom: 15,
-  });
-  new google.maps.Marker({
-    position: { lat: 44.434831044470364, lng: 26.147003026598945 },
-    map: map,
-    title: "RA Athletic",
-    Animation: google.maps.Animation.BOUNCE,
-  });
-}
-initMap();
+// let map;
+// async function initMap() {
+//   const { Map } = await google.maps.importLibrary("maps");
+//   map = new Map(document.getElementById("map"), {
+//     center: { lat: 44.434831044470364, lng: 26.147003026598945 },
+//     zoom: 15,
+//   });
+//   new google.maps.Marker({
+//     position: { lat: 44.434831044470364, lng: 26.147003026598945 },
+//     map: map,
+//     title: "RA Athletic",
+//     Animation: google.maps.Animation.BOUNCE,
+//   });
+// }
+// initMap();
